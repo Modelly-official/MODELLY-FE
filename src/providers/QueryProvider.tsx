262b91eb55e4a6d333194c926834b7a245d6ea39
fkgrkyr <fkgrkyr@gmail.com>
2025-12-19 22:53:29 +0900

    #8 [FEAT] Query Client 및 Provider 설정

---
+++ src/providers/QueryProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useState } from "react";
+import { makeQueryClient } from "@/src/lib/queryClient";
+
+export function QueryProvider({ children }: { children: React.ReactNode }) {
+  // queryClient가 컴포넌트 생명주기 동안 유지
+  const [queryClient] = useState(() => makeQueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
