6ae4850030335dc1e9c926db1c9470cb1d6f02b3
fkgrkyr <fkgrkyr@gmail.com>
2025-12-22 17:43:06 +0900

    #15 [FEAT] Toaster Provider 설정 및 Figma 디자인 스타일 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import '@/src/styles/globals.css';
 import { QueryProvider } from '@/src/providers/QueryProvider';
+import { Toaster } from 'sonner';
 
 export const metadata: Metadata = {
   title: 'Monde',
@@ -21,6 +22,17 @@ const RootLayout = ({
         <QueryProvider>
           <div className="mx-auto min-h-screen w-full overflow-x-hidden sm:w-[375px] sm:shadow-2xl">{children}</div>
         </QueryProvider>
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            unstyled: true,
+            classNames: {
+              toast: 'bg-black text-white text-body-2-medium px-6 py-3 rounded-full shadow-lg',
+              title: 'text-white',
+              description: 'text-white',
+            },
+          }}
+        />
       </body>
     </html>
   );
