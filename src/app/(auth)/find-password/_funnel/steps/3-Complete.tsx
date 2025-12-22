e74426d1543d6a6519e9b2b099b84ad5efc32017
fkgrkyr <fkgrkyr@gmail.com>
2025-12-22 16:58:57 +0900

    #15 [FEAT] 비밀번호 찾기 완료 화면 하단 버튼 반응형 적용

--- src/app/(auth)/find-password/_funnel/steps/3-Complete.tsx
+++ src/app/(auth)/find-password/_funnel/steps/3-Complete.tsx
@@ -17,18 +17,15 @@ export const StepComplete: React.FC = () => {
           <SignupCompletedIcon />
         </div>
         <div className="flex flex-col items-center gap-1">
-          <p className="text-gray-900 text-head-3-semibold text-center tracking-tight">
-            비밀번호 변경이 완료되었어요
-          </p>
+          <p className="text-gray-900 text-head-3-semibold text-center tracking-tight">비밀번호 변경이 완료되었어요</p>
         </div>
       </div>
       <button
         onClick={handleGoToLogin}
-        className="fixed bottom-[42px] left-1/2 -translate-x-1/2 w-[343px] py-4 bg-gray-900 text-white rounded-full text-body-1-medium cursor-pointer tracking-tight"
+        className="fixed bottom-[42px] left-1/2 -translate-x-1/2 w-[calc(100%-2rem)] sm:w-[343px] py-4 bg-gray-900 text-white rounded-full text-body-1-medium cursor-pointer tracking-tight"
       >
         로그인 화면으로 이동
       </button>
     </div>
   );
 };
-
