5150550d67978a8f0e3fc30c496e2a50c7438d2a
fkgrkyr <fkgrkyr@gmail.com>
2025-12-24 04:00:08 +0900

    #25 [FIX] find-id, find-pw 관련 ui 수정

--- src/components/auth/common/AuthHeader.tsx
+++ src/components/auth/common/AuthHeader.tsx
@@ -8,8 +8,8 @@ interface AuthHeaderProps {
  * 뒤로가기 버튼이 있는 인증 페이지 공통 헤더
  */
 export const AuthHeader: React.FC<AuthHeaderProps> = ({ onBack }) => (
-  <div className="mx-4 mt-15">
-    <button type="button" onClick={onBack} className="flex h-6 w-6 cursor-pointer items-center justify-center">
+  <div className="mx-4 flex h-14 items-center">
+    <button type="button" onClick={onBack} className="h-6 w-6 cursor-pointer">
       <LeftArrowIcon />
     </button>
   </div>
