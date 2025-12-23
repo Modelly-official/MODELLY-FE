fb7e36af8514e253934f321b384f87de5caff981
fkgrkyr <fkgrkyr@gmail.com>
2025-12-24 00:47:24 +0900

    #25 [FIX] 회원가입 페이지 상단(Title, header) 1-Terms  MARGIN 조정

--- src/components/signup/common/SignupHeader.tsx
+++ src/components/signup/common/SignupHeader.tsx
@@ -8,11 +8,13 @@ interface SignupHeaderProps {
 
 export const SignupHeader: React.FC<SignupHeaderProps> = ({ onBack, totalSteps, currentStep }) => {
   return (
-    <div className="mx-4 mt-15">
-      <button type="button" onClick={onBack} className="flex h-6 w-6 cursor-pointer items-center justify-center">
-        <LeftArrowIcon />
-      </button>
-      <div className="mt-4 flex gap-3">
+    <div className="mx-4">
+      <div className="flex h-13 items-center">
+        <button type="button" onClick={onBack} className="flex h-6 w-6 cursor-pointer items-center justify-center">
+          <LeftArrowIcon />
+        </button>
+      </div>
+      <div className="mt-1 flex gap-3">
         {Array.from({ length: totalSteps }).map((_, idx) => (
           <div key={idx} className={`h-1 flex-1 rounded ${idx < currentStep ? 'bg-blue-500' : 'bg-gray-300'}`} />
         ))}
--- src/components/signup/common/SignupTitle.tsx
+++ src/components/signup/common/SignupTitle.tsx
@@ -5,7 +5,7 @@ interface SignupTitleProps {
 
 export const SignupTitle: React.FC<SignupTitleProps> = ({ line1, line2 }) => {
   return (
-    <div className="mt-12 ml-4">
+    <div className="mt-8 ml-4">
       <p className="text-head-3-semibold mb-0 tracking-tight text-black">{line1}</p>
       {line2 && <p className="text-head-3-semibold mb-0 tracking-tight text-black">{line2}</p>}
     </div>
