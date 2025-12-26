9273a7601df2c665a8e6f23293267f09539deee6
fkgrkyr <fkgrkyr@gmail.com>
2025-12-26 20:48:06 +0900

    #32 [FEAT] 디자이너 레이아웃 및 role 기반 BottomNav 구현

---
+++ src/app/(designer)/layout.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { BottomNav } from '@/src/components/common';
+
+export default function DesignerLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  // 공고 상세/생성/수정 페이지에서는 하단 네비게이션 높이 padding 제외
+  const isDetailOrFormPage =
+    pathname.match(/^\/myRecruitment\/\d+/) || pathname === '/myRecruitment/create';
+
+  return (
+    <>
+      {/* 메인 컨텐츠 */}
+      <main className={isDetailOrFormPage ? '' : 'pb-[60px]'}>{children}</main>
+
+      {/* 하단 네비게이션 */}
+      <BottomNav />
+    </>
+  );
+}
