fa592060a92a0165572b0990ce54690e960697ff
fkgrkyr <fkgrkyr@gmail.com>
2025-12-29 15:43:39 +0900

    #35 [FIX] ConfirmModal 로딩중 overlay 클릭시 닫힘 방지

--- src/components/common/Modal/ConfirmModal.tsx
+++ src/components/common/Modal/ConfirmModal.tsx
@@ -92,7 +92,7 @@ export default function ConfirmModal({
   return (
     <div
       className="fixed inset-0 z-50 flex items-center justify-center bg-[rgba(28,28,30,0.4)]"
-      onClick={onClose}
+      onClick={() => !isLoading && onClose()}
       role="presentation"
     >
       <div
