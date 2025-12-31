bd6723b49ecae9f6c8c695b478aac8e507bbd02a
fkgrkyr <fkgrkyr@gmail.com>
2026-01-01 01:25:21 +0900

    #44 [REFACTOR] 미사용 DetailContent 컴포넌트 삭제

--- src/components/post/Content/index.ts
+++ src/components/post/Content/index.ts
@@ -1,5 +1,4 @@
 export { default as PostTabs } from './PostTabs';
-export { default as DetailContent } from './DetailContent';
 export { default as ServiceTags } from './ServiceTags';
 export { default as AvailableDates } from './AvailableDates';
 export { default as InfoSection } from './InfoSection';
