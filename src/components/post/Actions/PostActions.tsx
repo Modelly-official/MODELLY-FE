3f3d3b4b957366bf176e2a4514f420880a29f5c0
fkgrkyr <fkgrkyr@gmail.com>
2026-01-03 00:23:24 +0900

    #49 [FEAT] 채팅, 예약 버튼에 cursor-pointer 추가

--- src/components/post/Actions/PostActions.tsx
+++ src/components/post/Actions/PostActions.tsx
@@ -21,19 +21,19 @@ export default function PostActions({ recruitmentId, designerId }: PostActionsPr
   };
 
   return (
-    <div className="fixed bottom-0 left-1/2 z-50 w-full -translate-x-1/2 border-t border-gray-100 bg-white px-4 pb-2 pt-3 sm:w-[375px]">
+    <div className="fixed bottom-0 left-1/2 z-50 w-full -translate-x-1/2 border-t border-gray-100 bg-white px-4 pt-3 pb-2 sm:w-[375px]">
       <div className="flex gap-2">
         <button
           type="button"
           onClick={handleChat}
-          className="flex-1 rounded-full border border-gray-400 py-4 text-body-1-semibold text-gray-900"
+          className="text-body-1-semibold flex-1 cursor-pointer rounded-full border border-gray-400 py-4 text-gray-900"
         >
           채팅하기
         </button>
         <button
           type="button"
           onClick={handleReservation}
-          className="flex-1 rounded-full bg-gray-900 py-4 text-body-1-semibold text-white"
+          className="text-body-1-semibold flex-1 cursor-pointer rounded-full bg-gray-900 py-4 text-white"
         >
           예약하기
         </button>
@@ -41,4 +41,3 @@ export default function PostActions({ recruitmentId, designerId }: PostActionsPr
     </div>
   );
 }
-
