4f734f4a2d4613a1d2396fea1a08ef5713861d2f
fkgrkyr <fkgrkyr@gmail.com>
2026-01-13 20:58:58 +0900

    #69 [FIX] 리뷰 페이지 헤더에서 BellIcon 제거 및 타이틀 중앙 정렬

--- src/app/(common)/mypage/reviews/_components/DesignerReviewsPage.tsx
+++ src/app/(common)/mypage/reviews/_components/DesignerReviewsPage.tsx
@@ -3,7 +3,6 @@
 import { useState, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
 import ArrowLeftIcon from '@/public/icons/common/arrow-left.svg';
-import BellIcon from '@/public/icons/designer-home/bell.svg';
 import { useDesignerReviews, useCreateReply, useUpdateReply, usePinReview } from '@/src/hooks/queries/review';
 import { useInfiniteScroll } from '@/src/hooks/common/useInfiniteScroll';
 import { DesignerReviewCard } from '@/src/components/mypage/designer-reviews';
@@ -101,13 +100,7 @@ export default function DesignerReviewsPage() {
           <ArrowLeftIcon className="text-black" />
         </button>
         <h1 className="text-head-4-medium text-center text-black">리뷰 관리</h1>
-        <button
-          type="button"
-          className="flex size-6 cursor-pointer items-center justify-center"
-          aria-label="알림"
-        >
-          <BellIcon className="text-black" />
-        </button>
+        <div className="size-6" />
       </header>
 
       {/* 전체 개수 */}
