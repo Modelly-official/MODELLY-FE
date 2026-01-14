4eff81c7177feff3101162e40548908555d9b16e
fkgrkyr <fkgrkyr@gmail.com>
2026-01-14 23:24:18 +0900

    #72 [REFACTOR] 미사용 useVerticalDrag hook 삭제

--- src/hooks/custom/map/index.ts
+++ src/hooks/custom/map/index.ts
@@ -2,4 +2,3 @@ export { useMapState } from './useMapState';
 export { useMapFilters } from './useMapFilters';
 export { useSearchCenter } from './useSearchCenter';
 export { useMapData } from './useMapData';
-export { useVerticalDrag } from './useVerticalDrag';
