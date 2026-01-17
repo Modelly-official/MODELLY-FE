3b1b33c6384b1f109c1e80bd43357ef2ba209a04
fkgrkyr <fkgrkyr@gmail.com>
2026-01-17 19:40:17 +0900

    #77 [FEAT] 마감 공고 리스트 컴포넌트 추가

---
+++ src/components/myRecruitment/List/ClosedRecruitmentList.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import type { MyRecruitmentListItem } from '@/src/types/myRecruitment/recruitment';
+
+import ClosedRecruitmentListItem from './ClosedRecruitmentListItem';
+import RecruitmentEmpty from './RecruitmentEmpty';
+
+interface ClosedRecruitmentListProps {
+  recruitments: MyRecruitmentListItem[];
+  totalCount: number;
+  onClick?: (id: number) => void;
+}
+
+export default function ClosedRecruitmentList({
+  recruitments,
+  totalCount,
+  onClick,
+}: ClosedRecruitmentListProps) {
+  if (recruitments.length === 0) {
+    return <RecruitmentEmpty />;
+  }
+
+  return (
+    <div className="flex flex-col gap-3 px-4 pt-6">
+      {/* 전체 카운트 */}
+      <div className="flex items-center gap-1">
+        <span className="text-body-2-medium text-black">전체</span>
+        <span className="text-body-2-semibold text-gray-600">{totalCount}</span>
+      </div>
+
+      {/* 리스트 */}
+      <div className="flex flex-col gap-4">
+        {recruitments.map((recruitment) => (
+          <ClosedRecruitmentListItem
+            key={recruitment.recruitmentId}
+            recruitment={recruitment}
+            onClick={onClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
