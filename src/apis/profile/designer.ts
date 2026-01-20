81af2ba0a4f5512559147272736c9264d5b76ff1
jungyungee <jungyungee0610@gmail.com>
2026-01-20 13:15:51 +0900

    #62 [FEAT] 디자이너 본인 프로필 조회 API 추가

--- src/apis/profile/designer.ts
+++ src/apis/profile/designer.ts
@@ -14,3 +14,12 @@ export async function getPublicDesignerProfile(
   );
   return data;
 }
+
+/**
+ * 디자이너 본인 프로필 조회 (공개용 프로필)
+ * GET /designers/profiles
+ */
+export async function getMyDesignerProfile(): Promise<ApiResponse<DesignerProfileResponse>> {
+  const { data } = await axiosInstance.get<ApiResponse<DesignerProfileResponse>>('/designers/profiles');
+  return data;
+}
