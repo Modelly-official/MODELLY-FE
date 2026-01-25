8ada7439316afb10f6d9c278f67eba04c5d8ca01
fkgrkyr <fkgrkyr@gmail.com>
2026-01-25 23:20:30 +0900

    #140 [FIX] 상태바 default로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   },
   appleWebApp: {
     capable: true,
-    statusBarStyle: 'black-translucent',
+    statusBarStyle: 'default',
     title: 'Moandi',
   },
   openGraph: {
