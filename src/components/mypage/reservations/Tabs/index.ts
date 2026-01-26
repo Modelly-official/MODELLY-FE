899f2f6910e482cc46fcf8213d7b617790ef4632
fkgrkyr <fkgrkyr@gmail.com>
2026-01-26 16:38:58 +0900

    #143 [REFACTOR] Dropdown 공통 컴포넌트로 마이그레이션 및 미사용 컴포넌트 삭제

--- src/components/mypage/reservations/Tabs/index.ts
+++ src/components/mypage/reservations/Tabs/index.ts
@@ -1,3 +1,2 @@
 export { default as ReservationTabs } from './ReservationTabs';
 export { default as CategoryChips } from './CategoryChips';
-export { default as MonthDropdown } from './MonthDropdown';
